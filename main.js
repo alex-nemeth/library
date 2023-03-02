e394958b55b17181401febea6e0f54017788b729
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-03-03 00:43:07 +0100

    library functionality implemented (form hidden)

---
+++ main.js
@@ -0,0 +1,38 @@
+let myLibrary = [];
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+}
+
+function addBook() {
+    const title = document.querySelector(".title").value;
+    const author = document.querySelector(".author").value;
+    const pages = document.querySelector(".pages").value;
+    const read = document.querySelector(".read").checked;
+    myLibrary.push(new Book(title, author, pages, read));
+    render();
+}
+
+function render() {
+    let bookList = document.querySelector(".booklist");
+    bookList.innerHTML = "";
+    for (let i = 0; i < myLibrary.length; i++) {
+        const newCard = document.createElement("div");
+        newCard.innerHTML = `<p>Title: ${myLibrary[i].title}</p>
+        <p id="author">Author: ${myLibrary[i].author}</p>
+        <p id="pages">Pages: ${myLibrary[i].pages}</p>`;
+        newCard.classList.add(myLibrary[i].read);
+        bookList.appendChild(newCard);
+    }
+}
+
+const addBookBtn = document.querySelector(".addbook");
+addBookBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    addBook();
+});
